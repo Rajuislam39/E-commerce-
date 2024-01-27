9fd175faadca788ac6aa2245ac9ce33f7f7e0d69
raju-islam <rajuslam39@gmail.com>
2024-01-27 18:31:04 +0600

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,112 @@
 import 'package:flutter/material.dart';
 
+import 'src/utils/theme/theme.dart';
 
-
-
-
-
-void main(){
+void main() {
   runApp(kkk());
 }
-MaterialApp kkk(){
+
+MaterialApp kkk() {
   return MaterialApp(
     debugShowCheckedModeBanner: false,
-home: AppHome(),
+    theme: TAppTheme.lightTheme,
+
+    darkTheme: TAppTheme.darkTheme,
+    themeMode: ThemeMode.light,
+    home: AppHome(),
   );
 }
 
-class AppHome extends StatelessWidget {
+class AppHome extends StatefulWidget {
   const AppHome({super.key});
+
+  @override
+  State<AppHome> createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        title: const Text(".appable/"),leading: const Icon(Icons.video_collection_outlined),
+        title: const Text(".appable/"),
+        leading: const Icon(Icons.video_collection_outlined),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},child: const Icon(Icons.add_shopping_cart_outlined),
+        onPressed: () {},
+        child: const Icon(Icons.add_shopping_cart_outlined),
       ),
-      body: ListView(
-        children: [
-          const Text("Heading")
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Text(
+              "Heading",
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            Text(
+              "Sub-heading",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text(
+              "Paragraph",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Eleveted Button")),
+            SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 28,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.blue),
+                child: Center(
+                    child: Text(
+                  "Eleveted Button",
+                  style: TextStyle(color: Colors.white),
+                )),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+                height: 100,
+                child: OutlinedButton(
+                    onPressed: () {}, child: Text("Outline Button"))),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Password"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: Card(
+                  // color: Colors.teal,
+                  ),
+            )
+          ],
+        ),
       ),
     );
   }
